7d7960f8392aedb2d04509da84d23d72cf414a0c
yusei ogura <y-ogura@zyyx.jp>
2024-09-26 17:52:12 +0900

    一意でない値で検索している箇所を[パスパラメータ→クエリパラメータ]で持たせるよう修正

--- src/main/java/com/example/kakeibo/controller/ExpenseController.java
+++ src/main/java/com/example/kakeibo/controller/ExpenseController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -38,8 +39,8 @@ public class ExpenseController {
      * @param yearMonth リクエストパラメータ：年月
      * @return 支出リスト
      */
-    @GetMapping("/{yearMonth}")
-    public ResponseEntity<Object> getExpensesByMonth(@PathVariable String yearMonth) {
+    @GetMapping
+    public ResponseEntity<Object> getExpensesByMonth(@RequestParam String yearMonth) {
         if (yearMonth == null || yearMonth.isEmpty()) {
             return ResponseEntity.badRequest().body("対象月を入力してください");
         }
